2d808605617ab179fc5952bc832f63477bd8d7fc
MariaCsantosbela <104848675+MariaCsantosbela@users.noreply.github.com>
2022-07-13 22:09:54 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,26 @@
+const BASE_URL ='https://thatcopy.pw/catapi/rest/';
+const catBtn= document.getElementById('change-cat');
+
+
+
+const getCats = async() =>{
+    try{
+    const data = await fetch(BASE_URL);
+    const json = await data.json();
+    return json.webpurl;
+
+}
+catch (e) {
+    console.log(e.message);
+}
+};
+
+const loadImg = async() =>{
+ const catImag = document.getElementById('cat');
+ catImag.src = await getCats();
+
+};
+
+catBtn.addEventListener('click', loadImg);
+
+loadImg();
